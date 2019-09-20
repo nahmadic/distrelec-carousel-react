ee59709798ce449afd493ea8fd39ed0ab53980c7
Ryan Browne <brownetown7@gmail.com>
2019-09-20 20:43:18 +0100

    Adds component structure

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,24 +2,11 @@ import React from 'react';
 
 import './App.scss';
 
-import logo from '../../assets/images/logo.svg';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1 className="header__title">Distrelec Carousel</h1>
       </header>
     </div>
   );
