6e9af541bc865c74ed63f89f563cf3d364569ec2
Ryan Browne <brownetown7@gmail.com>
2019-09-21 13:31:56 +0100

    Adds previous and next slide functionality

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Carousel.scss';
 import Slide from 'components/slide/Slide';
 
-function Carousel({ slideData }) {
+function Carousel ({ slideData }) {
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-  console.log(slideData);
+  const length = slideData.length;
+
+  const previousSlide = () => {
+    const lastSlide = slideData.length - 1;
+    const shouldResetSlide = currentSlide === 0;
+    const newSlide =  shouldResetSlide ? lastSlide : currentSlide - 1;
+
+    setCurrentSlide(newSlide);
+  }
+
+  const nextSlide = () => {
+    const lastSlide = slideData.length - 1;
+    const shouldResetSlide = currentSlide === lastSlide;
+    const newSlide =  shouldResetSlide ? 0 : currentSlide + 1;
+
+    setCurrentSlide(newSlide);
+  }
 
   return (
-    <div className="carousel">
-      <div className="carousel__button carousel__button--prev"></div>
-      <div className="carousel__inner">
-        <div className="carousel__track">
-          {slideData.map(slide => <Slide key={slide.code} slideData={slide}/>)}
+    length > 0 && (
+      <div className="carousel">
+        <div className="carousel__button carousel__button--prev"></div>
+        <div className="carousel__inner">
+          <div className="carousel__track">
+            {slideData.map((slide, index) => <Slide key={index} slideData={slide}/>)}
+          </div>
         </div>
+        <div className="carousel__button carousel__button--next"></div>
       </div>
-      <div className="carousel__button carousel__button--next"></div>
-    </div>
+    )
   );
 }
 
