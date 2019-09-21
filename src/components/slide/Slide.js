20ea2c2462a8e4c0d255d7b871cab2092763e17f
Ryan Browne <brownetown7@gmail.com>
2019-09-21 11:56:59 +0100

    Adds basic slide structure

--- src/components/slide/Slide.js
+++ src/components/slide/Slide.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 import './Slide.scss';
 
-function Slide({ code }) {
+function Slide({ slideData }) {
+  const { name, url, productImageUrl, productImageAltText, price } = slideData;
 
   return (
-    <div className="Slide">
-      <h3>Slide {code} goes here</h3>
+    <div className="slide">
+      <div className="slide__inner">
+        <img className="slide__image" src={productImageUrl} alt={productImageAltText} />
+        <h3 className="slide__title">{name}</h3>
+        <h5 className="slide__price">{price.currency} {price.formattedValue}</h5>
+        <a href={url} className="slide__button">Buy now</a>
+      </div>
     </div>
   );
 }
